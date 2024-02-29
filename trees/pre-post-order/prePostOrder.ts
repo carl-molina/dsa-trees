49fded4d5572b8d9ade8bf5fe833e38900b47bac
Veronica Ni <vni1294@gmail.com>
2024-02-28 20:08:40 -0500

    finished preOrder

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -1,18 +1,27 @@
-import { BNodeNum } from "../common/binary-search-tree";
+import { Tree, TreeNodeNum } from "../common/tree";
 
 /** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
  * Returns an array of visited nodes. */
 
-function preOrder(node: BNodeNum | null): number[] {
-  return [42];
+function preOrder(node: TreeNodeNum | null): number[] {
+  if (!node) return [];
+  if (node.children.length === 0) {
+    return [node.val];
+  }
+  let nums = [node.val];
+  for (const c of node.children) {
+    nums = nums.concat(preOrder(c));
+  }
+  
+  return nums;
 }
 
 
 /** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
  * Returns an array of visited nodes. */
 
-function postOrder(node: BNodeNum | null): number[] {
+function postOrder(node: TreeNodeNum | null): number[] {
   return [42];
 }
 
-export { preOrder, postOrder };
+export { preOrder, postOrder }; 
